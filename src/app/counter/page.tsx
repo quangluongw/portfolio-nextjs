500618b67d6412ac1b28bc79b53b2299acfad74b
name22334 <quang20042204@gmail.com>
2025-05-12 16:28:41 +0700

    cấu hình vercel

--- src/app/counter/page.tsx
+++ src/app/counter/page.tsx
@@ -3,7 +3,6 @@ import Couter from "./counter";
 export const metadata = {
     title: "dd"
 }
-
 export default function CouterPage() {
     return (
         <Couter />
--- src/supabase/supabase.tsx
+++ src/supabase/supabase.tsx
@@ -1,5 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://mqaesyeqvgxzzdgcyspw.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1xYWVzeWVxdmd4enpkZ2N5c3B3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3OTc1NzUsImV4cCI6MjA2MjM3MzU3NX0.oNZSuBo6CW8xDGYcNbQS-u6WxUuNdx8EfM0OzdtbPiY";
-const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl!, supabaseKey!);
+
 export default supabase;

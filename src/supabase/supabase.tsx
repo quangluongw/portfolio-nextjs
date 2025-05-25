467481730014e1440d3de14defcee6183bb265f2
quangluongw <quang20042204@gmail.com>
2025-05-25 17:15:56 +0700

    fix

--- src/supabase/supabase.tsx
+++ src/supabase/supabase.tsx
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Lấy URL và Anon Key từ biến môi trường
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl!, supabaseAnonKey!)

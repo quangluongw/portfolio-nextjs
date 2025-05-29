5ec85cd0eaccb2cc9cd34a1f9455382c0dccf0d2
quangluongw <quang20042204@gmail.com>
2025-05-29 11:38:14 +0700

    fix supbase

--- src/app/layout2.tsx
+++ src/app/layout2.tsx
@@ -1,8 +1,8 @@
-'use client';
+"use client";
 import { Geist, Geist_Mono } from "next/font/google";
-import Footer from '../components/common/Footer';
+import Footer from "../components/common/Footer";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import Header from "@/components/common/Header";
 
 const geistSans = Geist({
@@ -21,27 +21,30 @@ export default function LayoutClient({
   children: React.ReactNode;
 }) {
   const queryClient = new QueryClient();
-  const [theme, setTheme] = useState(''); // Giá trị mặc định
+  const [theme, setTheme] = useState("");
 
   useEffect(() => {
-    // Chỉ chạy trên client side
-    const savedTheme = localStorage.getItem('theme') || 'dark';
+    const savedTheme = localStorage.getItem("theme") || "dark";
     setTheme(savedTheme);
   }, []);
 
   return (
     <html lang="en" data-theme={theme}>
       <head>
-        <script dangerouslySetInnerHTML={{
-          __html: `
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
             document.documentElement.setAttribute('data-theme', 
               localStorage.getItem('theme') || 'dark'
             );
           `,
-        }} />
+          }}
+        />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${theme === null ? 'opacity-0' : 'opacity-100'}  max-w-screen-xl mx-auto my-3 px-3 md:px-8 antialiased dark:bg-[#181A1F] dark:text-white`}
+        className={`${geistSans.variable} ${geistMono.variable} ${
+          theme === null ? "opacity-0" : "opacity-100"
+        }  max-w-screen-xl mx-auto my-3 px-3 md:px-8 antialiased dark:bg-[#181A1F] dark:text-white`}
       >
         <QueryClientProvider client={queryClient}>
           <Header />
--- src/supabase/supabase.tsx
+++ src/supabase/supabase.tsx
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Lấy URL và Anon Key từ biến môi trường
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
+const supabaseUrl = "https://mqaesyeqvgxzzdgcyspw.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1xYWVzeWVxdmd4enpkZ2N5c3B3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3OTc1NzUsImV4cCI6MjA2MjM3MzU3NX0.oNZSuBo6CW8xDGYcNbQS-u6WxUuNdx8EfM0OzdtbPiY"
 
 export const supabase = createClient(supabaseUrl!, supabaseAnonKey!)
